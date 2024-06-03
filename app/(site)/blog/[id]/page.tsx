98437635e30921d03fbbb7a282bd43baf0f3ab8f
Evgeny <vt211_fyeo@student.ztu.edu.ua>
2024-06-04 01:05:33 +0300

    Hero component & added picture

---
+++ app/(site)/blog/[id]/page.tsx
@@ -0,0 +1,23 @@
+interface searchParamsType {
+    id: string;
+    title: string;
+    image_path: string;
+    paragraph: string;
+    featured: boolean;
+    topPost: boolean;
+    tags: string;
+    authorImage: string;
+    authorName: string;
+    publishDate: string;
+}
+
+const page = ({searchParams}: {searchParams: searchParamsType}) => {
+    const post = searchParams;
+    return (
+        <div>
+            {post.title}
+        </div>
+    )
+};
+
+export default page;
