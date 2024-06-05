082933ad71c12e04952569559315085557ecd5bb
Evgeny <vt211_fyeo@student.ztu.edu.ua>
2024-06-05 17:54:41 +0300

    Post page styling

--- app/(site)/blog/[id]/page.tsx
+++ app/(site)/blog/[id]/page.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import Tag from "@/components/ui/Tag"
+import {AiOutlineTwitter, AiOutlineInstagram, AiOutlineFacebook} from "react-icons/ai";
+
 interface searchParamsType {
     id: string;
     title: string;
@@ -5,17 +9,58 @@ interface searchParamsType {
     paragraph: string;
     featured: boolean;
     topPost: boolean;
-    tags: string;
+    tags: string[];
     authorImage: string;
     authorName: string;
     publishDate: string;
 }
 
-const page = ({searchParams}: {searchParams: searchParamsType}) => {
+const page = ({searchParams}: { searchParams: searchParamsType }) => {
     const post = searchParams;
     return (
-        <div>
-            {post.title}
+        <div className="w-[95%] mx-auto max-w-[1450px]">
+            <div className="w-full h-[400px] relative mb-5">
+                <Image src={post.image_path} fill alt="image for blog" className="object-cover"/>
+            </div>
+            <Tag text={post.tags}/>
+            <h2 className="text-4xl font-extrabold uppercase text-tertiary my-3">
+                {post.title}
+            </h2>
+            <div className="flex md:gap-20 gap-5 relative mt-10 md:flex-row flex-col">
+                <aside className="md:sticky md:top-0 md:h-screen">
+                    <span className="uppercase text-2xl font-extrabold text-tertiary">
+                        Share:
+                    </span>
+                    <div className="flex text-3xl gap-5 text-gray-400 mt-2">
+                        <AiOutlineFacebook/>
+                        <AiOutlineInstagram/>
+                        <AiOutlineTwitter/>
+                    </div>
+                </aside>
+                <article>
+                    <p className="text-xl">
+                        {post.paragraph}
+                        {post.paragraph}
+                        {post.paragraph}
+                        {post.paragraph}
+                        {post.paragraph}
+                        {post.paragraph}
+                        {post.paragraph}
+                    </p>
+                    <div className="mt-5 flex gap-5 items-center">
+                        <Image src={post.authorImage} alt={`Image of ${post.authorName}`} width={500} height={500}
+                               className="rounded-full w-20 h-20 object-cover"/>
+                        <div className="flex ga-1 flex-col">
+                            <span>
+                                {post.authorName}
+                            </span>
+                            <span>
+                                {post.publishDate}
+                            </span>
+                        </div>
+                    </div>
+                </article>
+            </div>
         </div>
     )
 };
