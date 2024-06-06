5322ac2b0eb3cea3606046d0c4cddce7f55fde4a
Evgeny <vt211_fyeo@student.ztu.edu.ua>
2024-06-07 00:20:02 +0300

    Contact page & few fixes

---
+++ components/ui/Map.tsx
@@ -0,0 +1,11 @@
+const Map = () => {
+    return (
+        <>
+            <div style={{width: "100%"}}>
+                <iframe width="100%" height="300" src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=Zhytomyr%20Polytechnic+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+            </div>
+        </>
+    )
+};
+
+export default Map;
