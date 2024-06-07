10f9ca49e650e58787ae4fb18fe1e130c9aad13d
Evgeny <vt211_fyeo@student.ztu.edu.ua>
2024-06-08 01:39:03 +0300

    NextAuth, Prisma & MongoDB setup

---
+++ app/(auth)/access/page.tsx
@@ -0,0 +1,23 @@
+"use client"
+import {useState, useEffect} from "react";
+import {signIn, useSession} from "next-auth/react";
+import {useRouter} from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+
+const Page = () => {
+    const session = useSession()
+    const router = useRouter()
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        if (session?.status === "authenticated"){
+            console.log("Authenticated")
+            router.push("/")
+        }
+    }, [session?.status, router]);
+
+    return <div>Page</div>
+};
+
+export default Page;
