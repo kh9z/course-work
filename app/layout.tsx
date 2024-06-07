69b6521835869df9fe55ed414916b61daa0a5e57
Evgeny <vt211_fyeo@student.ztu.edu.ua>
2024-06-08 02:27:01 +0300

    Custom signup page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import {Roboto} from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
+import AuthContext from "@/context/AuthContext";
 
 const roboto = Roboto({subsets: ["latin"], weight: ["100", "400", "700", "900"]});
 
@@ -18,11 +19,13 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="ua">
-        <body className={`${roboto.className} overflow-x-hidden bg-light`}>
-        <Navbar/>
-        {children}
-        <Footer/>
-        </body>
+        <AuthContext>
+            <body className={`${roboto.className} overflow-x-hidden bg-light`}>
+            <Navbar/>
+            {children}
+            <Footer/>
+            </body>
+        </AuthContext>
         </html>
     );
 }
